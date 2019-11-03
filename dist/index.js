dfd51f0bf5f835c1a369e41aaccefe3b2cba6ceb
Artis3n <dev@quantummadness.com>
2019-11-02 20:27:00 -0400

    Class is causing issues, removing

--- dist/index.js
+++ dist/index.js
@@ -1186,44 +1186,32 @@ const exec = __webpack_require__(986);
 const yaml = __webpack_require__(414);
 const fs = __webpack_require__(747);
 
-class AnsibleCollection {
-    constructor(namespace, name, version) {
-        this.namespace = this._validateParam(namespace);
-        this.name = this._validateParam(name);
-        this.version = this._validateParam(version);
-    }
-
-    get title() {
-        return `${this.namespace}-${this.name}-${this.version}`;
-    }
-
-    _validateParam(param) {
-        if (param !== undefined) {
-            return param;
-        }
-        core.error("galaxy.yml is missing required namespace, name, or version parameter.");
-        throw new Error("Missing required parameters in galaxy.yml");
-    }
-}
-
 try {
     const apiKey = core.getInput('api_key', { required: true });
     const galaxy_config_file = core.getInput('galaxy_config_file') || 'galaxy.yml';
     const galaxy_config = yaml.safeLoad(fs.readFileSync(galaxy_config_file, 'utf8'));
 
-    const collection = new AnsibleCollection(galaxy_config.namespace, galaxy_config.name, galaxy_config.version);
+    const namespace = galaxy_config.namespace;
+    const name = galaxy_config.name;
+    const version = galaxy_config.version;
 
-    core.debug(`Building collection ${collection.title}`);
-    buildCollection(collection, apiKey)
-        .then(() => core.debug(`Successfully published ${collection.title} to Ansible Galaxy.`))
+    if (namespace === undefined || name === undefined || version === undefined) {
+        const error = new Error("Missing require namespace, name, or version fields in galaxy.yml");
+        core.error(error.message);
+        core.setFailed(error.message);
+    }
+
+    core.debug(`Building collection ${namespace}-${name}-${version}`);
+    buildCollection(namespace, name, version, apiKey)
+        .then(() => core.debug(`Successfully published ${namespace}-${name} v${version} to Ansible Galaxy.`))
         .catch(err => core.setFailed(err.message));
 } catch (error) {
     core.setFailed(error.message);
 }
 
-async function buildCollection(collection, apiKey) {
+async function buildCollection(namespace, name, version, apiKey) {
     await exec.exec('ansible-galaxy collection build');
-    await exec.exec(`ansible-galaxy collection publish ${collection.title}.tar.gz --api-key=${apiKey}`)
+    await exec.exec(`ansible-galaxy collection publish ${namespace}-${name}-${version}.tar.gz --api-key=${apiKey}`)
 }
 
 

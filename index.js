1d73e4acfaf4a72a3fbafd01634f1f98264c192e
Artis3n <dev@quantummadness.com>
2019-11-02 20:16:56 -0400

    Moving the class

--- index.js
+++ index.js
@@ -3,26 +3,6 @@ const exec = require('@actions/exec');
 const yaml = require('js-yaml');
 const fs = require('fs');
 
-try {
-    const apiKey = core.getInput('api_key', { required: true });
-    const galaxy_config_file = core.getInput('galaxy_config_file') || 'galaxy.yml';
-    const galaxy_config = yaml.safeLoad(fs.readFileSync(galaxy_config_file, 'utf8'));
-
-    const collection = new AnsibleCollection(galaxy_config.namespace, galaxy_config.name, galaxy_config.version);
-
-    core.debug(`Building collection ${collection.title}`);
-    buildCollection(collection, apiKey)
-        .then(() => core.debug(`Successfully published ${collection.title} to Ansible Galaxy.`))
-        .catch(err => core.setFailed(err.message));
-} catch (error) {
-    core.setFailed(error.message);
-}
-
-async function buildCollection(collection, apiKey) {
-    await exec.exec('ansible-galaxy collection build');
-    await exec.exec(`ansible-galaxy collection publish ${collection.title}.tar.gz --api-key=${apiKey}`)
-}
-
 class AnsibleCollection {
     constructor(namespace, name, version) {
         this.namespace = this._validateParam(namespace);
@@ -41,4 +21,24 @@ class AnsibleCollection {
         core.error("galaxy.yml is missing required namespace, name, or version parameter.");
         throw new Error("Missing required parameters in galaxy.yml");
     }
+}
+
+try {
+    const apiKey = core.getInput('api_key', { required: true });
+    const galaxy_config_file = core.getInput('galaxy_config_file') || 'galaxy.yml';
+    const galaxy_config = yaml.safeLoad(fs.readFileSync(galaxy_config_file, 'utf8'));
+
+    const collection = new AnsibleCollection(galaxy_config.namespace, galaxy_config.name, galaxy_config.version);
+
+    core.debug(`Building collection ${collection.title}`);
+    buildCollection(collection, apiKey)
+        .then(() => core.debug(`Successfully published ${collection.title} to Ansible Galaxy.`))
+        .catch(err => core.setFailed(err.message));
+} catch (error) {
+    core.setFailed(error.message);
+}
+
+async function buildCollection(collection, apiKey) {
+    await exec.exec('ansible-galaxy collection build');
+    await exec.exec(`ansible-galaxy collection publish ${collection.title}.tar.gz --api-key=${apiKey}`)
 }

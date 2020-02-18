f8cabed88284c55d536a2273a47302cc2ac18d17
Ari Kalfus <dev@artis3nal.com>
2020-02-17 21:34:11 -0600

    Re-wrote into a Docker action (#31)
    * Re-writing into typescript inside a Docker action
    
    * Updating Dockerfile
    
    * Cleaning up husky
    
    * Updated distribution
    
    * 2.0.0
    
    * Works
    
    * Update actions used in workflows
    
    * Removed files generated by intellij

--- README.md
+++ README.md
@@ -8,16 +8,11 @@
 
 Deploy a Collection to Ansible Galaxy.
 
-v1 of this action is a JavaScript-based action. The upcoming v2 will be a Docker-based action. This will remove the Python dependency requirements in the example usage section, as all dependencies will be incorporated inside the Docker container.
-
 ## Requirements
 
 This action expects to be run from a repository with certain met conditions.
 
 1. The repository is an Ansible Galaxy Collection, meaning it contains a `galaxy.yml` file at the project root.
-1. This action is part of a Python workflow.
-    1. This means your workflow should include a step for `uses: actions/setup-python@v1`.
-    1. Before this action is called, ensure that `ansible` is installed (`pip install ansible`).
 
 An example workflow using this action can be found [here](https://github.com/artis3n/ansible-collection-github/blob/master/.github/workflows/deploy.yml).
 
@@ -38,18 +33,8 @@ A collection must have a galaxy.yml file that contains the necessary information
 ## Example Usage
 
 ```yaml
-- name: Set up Python 3
-  uses: actions/setup-python@v1
-  with:
-    python-version: 3.6
-
-- name: Install dependencies
-  run: |
-    python -m pip install --upgrade pip
-    pip install ansible  # Must be at least 2.9.0
-
 - name: Build and Deploy Collection
   uses: artis3n/ansible_galaxy_collection@v1
   with:
-    api_key: 'df328fawrfr32iuaw'
+    api_key: '${{ secrets.GALAXY_API_KEY }}'
 ```
---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { debug as coreDebug, error as coreError, getInput, setFailed } from '@actions/core';
+import { exec } from '@actions/exec';
+import { safeLoad } from 'js-yaml';
+import { readFileSync } from 'fs';
+
+import { GalaxyConfig } from './types';
+
+try {
+  const apiKey = getInput('api_key', { required: true });
+  const galaxy_config_file = getInput('galaxy_config_file') || 'galaxy.yml';
+  const galaxy_config: GalaxyConfig = safeLoad(readFileSync(galaxy_config_file, 'utf8'));
+
+  const namespace = galaxy_config.namespace;
+  const name = galaxy_config.name;
+  const version = galaxy_config.version;
+
+  if (namespace === undefined || name === undefined || version === undefined) {
+    const error = new Error('Missing require namespace, name, or version fields in galaxy.yml');
+    coreError(error.message);
+    setFailed(error.message);
+  } else {
+    coreDebug(`Building collection ${namespace}-${name}-${version}`);
+    buildCollection(namespace, name, version, apiKey)
+      .then(() =>
+        coreDebug(`Successfully published ${namespace}-${name} v${version} to Ansible Galaxy.`),
+      )
+      .catch(err => setFailed(err.message));
+  }
+} catch (error) {
+  setFailed(error.message);
+}
+
+async function buildCollection(namespace: string, name: string, version: number, apiKey: string) {
+  await exec('ansible-galaxy collection build');
+  await exec(
+    `ansible-galaxy collection publish ${namespace}-${name}-${version}.tar.gz --api-key=${apiKey}`,
+  );
+}

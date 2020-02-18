b756392031f6066e47be44381b591f3b1cd4f02b
Artis3n <dev@artis3nal.com>
2020-02-17 21:17:58 -0600

    Updating Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
-FROM node:slim
+FROM python:slim AS ansible
+RUN python -m pip install --upgrade pip
+RUN pip install --upgrade ansible
 
+FROM node:slim AS action
 COPY . .
 RUN npm install --production
-
 ENTRYPOINT ["node", "dist/index.js"]
--- dist/index.js
+++ dist/index.js
@@ -8,33 +8,36 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const core = require('@actions/core');
-const exec = require('@actions/exec');
-const yaml = require('js-yaml');
-const fs = require('fs');
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@actions/core");
+const exec_1 = require("@actions/exec");
+const js_yaml_1 = require("js-yaml");
+const fs_1 = require("fs");
 try {
-    const apiKey = core.getInput('api_key', { required: true });
-    const galaxy_config_file = core.getInput('galaxy_config_file') || 'galaxy.yml';
-    const galaxy_config = yaml.safeLoad(fs.readFileSync(galaxy_config_file, 'utf8'));
+    const apiKey = core_1.getInput('api_key', { required: true });
+    const galaxy_config_file = core_1.getInput('galaxy_config_file') || 'galaxy.yml';
+    const galaxy_config = js_yaml_1.safeLoad(fs_1.readFileSync(galaxy_config_file, 'utf8'));
     const namespace = galaxy_config.namespace;
     const name = galaxy_config.name;
     const version = galaxy_config.version;
     if (namespace === undefined || name === undefined || version === undefined) {
-        const error = new Error("Missing require namespace, name, or version fields in galaxy.yml");
-        core.error(error.message);
-        core.setFailed(error.message);
+        const error = new Error('Missing require namespace, name, or version fields in galaxy.yml');
+        core_1.error(error.message);
+        core_1.setFailed(error.message);
     }
-    core.debug(`Building collection ${namespace}-${name}-${version}`);
-    buildCollection(namespace, name, version, apiKey)
-        .then(() => core.debug(`Successfully published ${namespace}-${name} v${version} to Ansible Galaxy.`))
-        .catch(err => core.setFailed(err.message));
+    else {
+        core_1.debug(`Building collection ${namespace}-${name}-${version}`);
+        buildCollection(namespace, name, version, apiKey)
+            .then(() => core_1.debug(`Successfully published ${namespace}-${name} v${version} to Ansible Galaxy.`))
+            .catch(err => core_1.setFailed(err.message));
+    }
 }
 catch (error) {
-    core.setFailed(error.message);
+    core_1.setFailed(error.message);
 }
 function buildCollection(namespace, name, version, apiKey) {
     return __awaiter(this, void 0, void 0, function* () {
-        yield exec.exec('ansible-galaxy collection build');
-        yield exec.exec(`ansible-galaxy collection publish ${namespace}-${name}-${version}.tar.gz --api-key=${apiKey}`);
+        yield exec_1.exec('ansible-galaxy collection build');
+        yield exec_1.exec(`ansible-galaxy collection publish ${namespace}-${name}-${version}.tar.gz --api-key=${apiKey}`);
     });
 }

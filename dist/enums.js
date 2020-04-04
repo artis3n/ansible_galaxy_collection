d8cc567ad226ba266ef495e4a88d64b6a056123d
Artis3n <dev@artis3nal.com>
2020-04-04 17:46:40 -0400

    Setting up jest

---
+++ dist/enums.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ExitCodes;
+(function (ExitCodes) {
+    ExitCodes[ExitCodes["Ok"] = 0] = "Ok";
+    ExitCodes[ExitCodes["DeployFailed"] = 1] = "DeployFailed";
+    ExitCodes[ExitCodes["InvalidSemver"] = 2] = "InvalidSemver";
+})(ExitCodes = exports.ExitCodes || (exports.ExitCodes = {}));
